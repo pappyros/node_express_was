0e41fb94dd20ab8bf4231bd1b28fe18518096b67
Lohan <hsj5009@lgcns.com>
2022-05-24 13:57:45 +0900

    asdf

---
+++ rds_endpoint.sh
@@ -0,0 +1,5 @@
+aws lambda invoke --function-name Lohan_get_rds_endpoint --payload '{ "value" : "RDS"}' response.json
+sleep 5
+sed -i 's/"//g' response.json
+export endpoint=`cat response.json`
+sed -i "s/endpoint/$endpoint/g" dockerfile
